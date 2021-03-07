dd3a9d4e193c269bc7971565ce113fd33d3e7ac1
KlayverGontijoo <Klayvergontijo@outlook.com>
2021-03-07 01:15:21 -0300

    Crud finalizado

---
+++ Project1/Configuracao/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project1.Model;
+
+namespace Project1.Configuracao
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+
+    }
+}
+
